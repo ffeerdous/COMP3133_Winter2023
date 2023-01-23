0685926193510155a7968608012ae86d39149cc4
ffeerdous <ferdousazizi9@gmail.com>
2023-01-22 19:14:43 -0500

    lab 01

--- Lab01_101315548_COMP3133/index.js
+++ Lab01_101315548_COMP3133/index.js
@@ -1,1 +1,9 @@
-console.log("Lab 01")
+var fs = require("fs")
+
+fs.readFile("input_countries.csv", (err, data) => {
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log(data.toString())
+})

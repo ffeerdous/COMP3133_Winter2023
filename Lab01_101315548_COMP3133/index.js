08af0d43fcb922ed84e28071da2ad884b44ed8d5
ffeerdous <ferdousazizi9@gmail.com>
2023-01-22 20:38:17 -0500

    lab 01

--- Lab01_101315548_COMP3133/index.js
+++ Lab01_101315548_COMP3133/index.js
@@ -1,9 +1,33 @@
 var fs = require("fs")
+var csv = require('csv-parser')
 
-fs.readFile("input_countries.csv", (err, data) => {
-    if(err){
-        console.log(err)
-        return
+const csvfile = "input_countries.csv";
+const column = 'country'
+const canada = "Canada"
+const USA = "United States"
+
+//Deletes the existing file and creates a new one -- Canada txt file
+if(fs.existsSync("canada.txt")){
+    fs.unlinkSync("canada.txt")
+}
+fs.writeFileSync("canada.txt", '')
+
+//Deletes the existing file and creates a new one -- USA txt file
+if(fs.existsSync("usa.txt")){
+    fs.unlinkSync("usa.txt")
+}
+fs.writeFileSync("usa.txt", '')
+
+//Writes data from csv file into txt file -- Canada txt file
+fs.createReadStream(csvfile).pipe(csv()).on('data', (data) =>{
+    if(data[column] === canada){
+        fs.appendFileSync("canada.txt", JSON.stringify(data)+'\n');
     }
-    console.log(data.toString())
+})
+
+//Writes data from csv file into txt file -- USA txt file
+fs.createReadStream(csvfile).pipe(csv()).on('data', (data) =>{
+    if(data[column] === USA){
+        fs.appendFileSync("usa.txt", JSON.stringify(data)+'\n');
+    }
 })
